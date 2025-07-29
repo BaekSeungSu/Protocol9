80e53d4694195c972ceeffa558ca9f7267c46994
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-07-29 19:41:15 +0900

    WeaponBase 클래스 추가 (WeaponBase.h, WeaponBase.cpp)

---
+++ Source/Protocol9/Private/Weapons/WeaponBase.cpp
@@ -0,0 +1,20 @@
+#include "Weapons/WeaponBase.h"
+
+AWeaponBase::AWeaponBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Protocol9/Public/Weapons/WeaponBase.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class PROTOCOL9_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AWeaponBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
