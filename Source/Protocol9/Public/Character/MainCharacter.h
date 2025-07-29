af30344c7eaecfa12b91ef84c98bda2fc569e0b9
USSA <ussa9905@gmail.com>
2025-07-29 19:35:37 +0900

    파일경로 변경

---
+++ Source/Protocol9/Public/Character/MainCharacter.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+struct FInputActionValue;
+class UCameraComponent;
+
+UCLASS()
+class PROTOCOL9_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	
+public:
+	AMainCharacter();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComponent;
+
+	
+	virtual void BeginPlay() override;
+
+public:	
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	UFUNCTION()
+	void Fire(const FInputActionValue& Value);
+	UFUNCTION()
+	void Dash(const FInputActionValue& Value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& Value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& Value);
+	
+};
