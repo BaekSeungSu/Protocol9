dca24b85382c87441599dd4f89a5ffd6f8e62d91
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-07-30 17:43:21 +0900

    WeaponBase에 IWeaponInterface 구현 및 필수 함수(PrimaryFire, StopFire, Reload) 추가

--- Source/Protocol9/Public/Weapons/WeaponBase.h
+++ Source/Protocol9/Public/Weapons/WeaponBase.h
@@ -2,25 +2,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Weapons/WeaponInterface.h"
 #include "WeaponBase.generated.h"
 
 class UStaticMeshComponent;
 
 UCLASS()
-class PROTOCOL9_API AWeaponBase : public AActor
+class PROTOCOL9_API AWeaponBase : public AActor, public IWeaponInterface
 {
 	GENERATED_BODY()
 	
 public:	
 	AWeaponBase();
+	virtual void PrimaryFire_Implementation() override;
+	virtual void StopFire_Implementation() override;
+	virtual void Reload_Implementation() override;
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* StaticMeshComp;
-	
-public:	
-	virtual void Tick(float DeltaTime) override;
 
+private:
 };
