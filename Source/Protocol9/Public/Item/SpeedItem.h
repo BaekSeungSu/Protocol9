465d38c5f793dcf74b75498bc150d615bf4cce21
J_J <112606394+bear300811@users.noreply.github.com>
2025-07-30 20:36:05 +0900

    아이템 기능  추가

---
+++ Source/Protocol9/Public/Item/SpeedItem.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "Character/MainCharacter.h"
+#include "SpeedItem.generated.h"
+
+
+
+UCLASS()
+class PROTOCOL9_API ASpeedItem : public AItemBase
+{
+	GENERATED_BODY()
+public:
+	ASpeedItem();
+	UPROPERTY()
+	AMainCharacter* AffectedPlayer;
+	
+	virtual void EndEffect() override;
+
+	virtual void ActivateItem(AActor* Activator)override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Item")
+	float MultiSpeed;
+	
+};
