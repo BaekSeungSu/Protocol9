8a81961b7986e9f3d980a3f3dde090730d14a29a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-08-04 20:38:19 +0900

    WeaponBase 클래스에 재장전 로직 추가 및 발사 관련 함수 개선

--- Source/Protocol9/Private/Weapons/WeaponBase.cpp
+++ Source/Protocol9/Private/Weapons/WeaponBase.cpp
@@ -2,6 +2,8 @@
 #include "Components/StaticMeshComponent.h"
 #include "Character/MainPlayerController.h"
 #include "Weapons/Data/WeaponData.h"
+#include "UI/PlayerUIComponent.h"
+#include "Character/MainCharacter.h"
 #include "DrawDebugHelpers.h"
 
 AWeaponBase::AWeaponBase()
@@ -29,6 +31,16 @@ void AWeaponBase::StopFire_Implementation()
 void AWeaponBase::Reload_Implementation()
 {
 	IWeaponInterface::Reload_Implementation();
+	if (bIsReloading || CurrentAmmo == CurrentWeaponData->MagazineSize)
+	{
+		return;
+	}
+
+	bIsReloading = true;
+	UE_LOG(LogTemp, Warning, TEXT("Reload"));
+
+	GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this, &AWeaponBase::FinishReload, CurrentWeaponData->ReloadTime, false);
+	
 }
 
 
@@ -37,6 +49,7 @@ void AWeaponBase::BeginPlay()
 {
 	Super::BeginPlay();
 	LoadWeaponData();
+	
 }
 
 
@@ -48,28 +61,75 @@ void AWeaponBase::Tick(float DeltaTime)
 }
 void AWeaponBase::FireAction()
 {
+	if (!CanFire())
+	{
+		return;
+	}
+	CurrentAmmo--;
+	ApplyRecoil();
+	
 	if (CurrentWeaponData)
 	{
 		if (CurrentWeaponData->FireType == EFireType::HitScan)
 		{
 			FireHitScan();
 		}
+		else if (CurrentWeaponData->FireType == EFireType::Projectile)
+		{
+			FireProjectile();
+		}
 	}
 }
 
+bool AWeaponBase::CanFire() const
+{
+	if (bIsReloading || CurrentAmmo <=0 || CurrentWeaponData == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can not Fire"));
+		return false;
+	}
+	return true;
+}
+
+void AWeaponBase::ApplyRecoil()
+{
+	AController* OwnerController = GetOwner()->GetInstigatorController();
+	if (OwnerController)
+	{
+		APlayerController* PlayerController = Cast<APlayerController>(OwnerController);
+		if (PlayerController)
+		{
+			const float Pitch = -CurrentWeaponData->RecoilPitch;
+			const float Yaw = FMath::RandRange(-CurrentWeaponData->RecoilYaw, CurrentWeaponData->RecoilYaw);
+			PlayerController->AddPitchInput(Pitch);
+			PlayerController->AddYawInput(Yaw);
+		}
+
+		AMainCharacter* OwnerCharacter = Cast<AMainCharacter>(GetOwner());
+		if (OwnerCharacter)
+		{
+			UPlayerUIComponent* UIComponent = OwnerCharacter->FindComponentByClass<UPlayerUIComponent>();
+			if (UIComponent)
+			{
+				float CurrentAimSize = UIComponent->GetAimSize();
+				UIComponent->SetAimSize(CurrentAimSize + CurrentWeaponData->CrosshairRecoilAmount);
+			}
+		}
+	}
+}
+
+
+
 void AWeaponBase::FireHitScan()
 {
-	AMainPlayerController* OwnerController = Cast<AMainPlayerController>(GetOwner()->GetInstigatorController());
-	if (!OwnerController) return;
-
-	FVector EyeLocation;
-	FRotator EyeRotation;
-	OwnerController->GetPlayerViewPoint(EyeLocation, EyeRotation);
+	FVector FireDirection = CalculateHitScanDirection();
+	if (FireDirection.IsZero()) return;
 	
-	FVector FireDirection = EyeRotation.Vector();
-	FVector TraceStart = EyeLocation;
+	FVector TraceStart;
+	FRotator UnusedRotation;
+	GetOwner()->GetInstigatorController()->GetPlayerViewPoint(TraceStart,UnusedRotation);
 	FVector TraceEnd = TraceStart + FireDirection * CurrentWeaponData->Range;
-
+	
 	FCollisionQueryParams CollisionParams;
 	CollisionParams.AddIgnoredActor(GetOwner());
 	CollisionParams.AddIgnoredActor(this);
@@ -78,8 +138,7 @@ void AWeaponBase::FireHitScan()
 	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECC_Visibility, CollisionParams);
 
 	FVector MuzzleLocation = WeaponMesh->GetSocketLocation(TEXT("MuzzleSocket"));
-	DrawDebugPoint(GetWorld(), MuzzleLocation, 10.0f, FColor::Red, false, 1.0f);
-
+	
 	if (bHit)
 	{
 		DrawDebugLine(GetWorld(), MuzzleLocation, HitResult.ImpactPoint, FColor::Green, false, 2.0f, 0, 2.0f);
@@ -94,12 +153,46 @@ void AWeaponBase::FireHitScan()
 	
 }
 
+FVector AWeaponBase::CalculateHitScanDirection() const
+{
+	AController* OwnerController = GetOwner()->GetInstigatorController();
+	if (!OwnerController) return FVector::ZeroVector;
+
+	FVector EyeLocation;
+	FRotator EyeRotation;
+	OwnerController->GetPlayerViewPoint(EyeLocation, EyeRotation);
+
+	FVector FireDirection = EyeRotation.Vector();
+	if (CurrentWeaponData&& CurrentWeaponData->SpreadAngle > 0.0f)
+	{
+		const float ConeHalfAngleRad = FMath::DegreesToRadians(CurrentWeaponData->SpreadAngle * 0.5f);
+		FireDirection = FMath::VRandCone(FireDirection, ConeHalfAngleRad);
+	}
+	
+	return FireDirection;
+}
+
+void AWeaponBase::FireProjectile()
+{
+}
+
+
 void AWeaponBase::LoadWeaponData()
 {
 	if (!WeaponDataTable) return;
 	if (WeaponDataRowName.IsNone()) return;
 
 	CurrentWeaponData = WeaponDataTable->FindRow<FWeaponData>(WeaponDataRowName, TEXT(""));
+	CurrentAmmo = CurrentWeaponData->MagazineSize;
+}
+
+void AWeaponBase::FinishReload()
+{
+	if (!CurrentWeaponData) return;
+
+	CurrentAmmo = CurrentWeaponData->MagazineSize;
+	bIsReloading = false;
+	UE_LOG(LogTemp, Warning, TEXT("Reload Finish"));
 }
 
 
