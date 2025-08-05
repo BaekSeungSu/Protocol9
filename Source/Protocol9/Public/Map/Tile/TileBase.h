b12908fe5f4f4a5f7729c1d47528b6fa0aed1d66
hakunamatata9909 <jkichul35@gmail.com>
2025-08-05 17:25:07 +0900

    tilemanager 구현

--- Source/Protocol9/Public/Map/Tile/TileBase.h
+++ Source/Protocol9/Public/Map/Tile/TileBase.h
@@ -43,4 +43,11 @@ public:
 	// 중첩 방지 반경
 	UPROPERTY(EditAnywhere, Category = "Instance")
 	float SpawnRadius = 50.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Tile info")
+	int32 TileIndex;
+	
+	#if WITH_EDITOR
+		void PrintIndex ();
+	#endif
 };
