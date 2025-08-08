0914d74385d48805ad7d0a6433cb257f0c3ec66d
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-08-08 20:40:34 +0900

    Pistol PrimaryFire 로직 정리: 불필요한 FireAction 호출 및 로그 제거

--- Source/Protocol9/Private/Weapons/Weapon_Pistol.cpp
+++ Source/Protocol9/Private/Weapons/Weapon_Pistol.cpp
@@ -8,8 +8,7 @@ AWeapon_Pistol::AWeapon_Pistol()
 void AWeapon_Pistol::PrimaryFire_Implementation()
 {
 	Super::PrimaryFire_Implementation();
-	UE_LOG(LogTemp, Warning, TEXT("Pistol Fired!!"));
-	FireAction();
+	
 }
 
 void AWeapon_Pistol::Reload_Implementation()
