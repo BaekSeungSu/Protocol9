961a37c01c7aa63fb5418e54a864b3fd3ec6787b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-08-11 08:27:02 +0900

    Weapon_SMG 클래스 수정: 자동 발사 로직 추가 및 FireRate 기반 발사 딜레이 구현

--- Source/Protocol9/Private/Weapons/Weapon_SMG.cpp
+++ Source/Protocol9/Private/Weapons/Weapon_SMG.cpp
@@ -1,17 +1,24 @@
 #include "Weapons/Weapon_SMG.h"
+#include "Weapons/Data/WeaponData.h"
 
 AWeapon_SMG::AWeapon_SMG()
 {
+	bIsFullAuto = true;
 }
 
 void AWeapon_SMG::PrimaryFire_Implementation()
 {
-	Super::PrimaryFire_Implementation();
+	if (!CurrentWeaponData) return;
+		
+	
+	const float TimeBetweenShots = 60.0f / CurrentWeaponData->FireRate;
+	GetWorld()->GetTimerManager().SetTimer(FireTimerHandle, this, &AWeaponBase::FireAction, TimeBetweenShots, true, 0.0f);
+
 }
 
 void AWeapon_SMG::StopFire_Implementation()
 {
-	Super::StopFire_Implementation();
+	GetWorld()->GetTimerManager().ClearTimer(FireTimerHandle);
 }
 
 void AWeapon_SMG::Reload_Implementation()
