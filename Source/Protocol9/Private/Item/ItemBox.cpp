ff05bf57d186df77bb296906893c0082141fd162
J_J <bear3007@naver.com>
2025-08-11 11:58:09 +0900

    박스 히트 수정 및 근접공격 박스 히트 구현

--- Source/Protocol9/Private/Item/ItemBox.cpp
+++ Source/Protocol9/Private/Item/ItemBox.cpp
@@ -26,16 +26,16 @@ void AItemBox::BeginPlay()
 
 float AItemBox::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
 {
-	float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	//float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
 
-	CurrentHP -= ActualDamage;
+	CurrentHP -= DamageAmount;
 	CurrentHP = FMath::Clamp(CurrentHP, 0.0f, MaxHP);
 	if (CurrentHP == 0.0f)
 	{
 		OnDestroyed();
 	}
 
-	return ActualDamage;
+	return DamageAmount;
 }
 
 void AItemBox::OnDestroyed()
