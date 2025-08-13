39f2dbb81b4e1e0b2356e4e57fb6cee2cff04806
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-08-13 21:10:37 +0900

    Pickup_Weapon 클래스 업데이트: 무기 상호작용 로직 추가 및 TryConsume 함수 구현

--- Source/Protocol9/Private/Weapons/Pickup_Weapon.cpp
+++ Source/Protocol9/Private/Weapons/Pickup_Weapon.cpp
@@ -1,27 +1,75 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Weapons/Pickup_Weapon.h"
+#include "Weapons/WeaponBase.h"
+#include "Character/MainCharacter.h"
+#include "Components/SphereComponent.h"
+#include "Weapons/InventoryComponent.h"
 
-// Sets default values
 APickup_Weapon::APickup_Weapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	SphereCollision->SetupAttachment(GetRootComponent());
+	SphereCollision->InitSphereRadius(120.f);
+	SphereCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereCollision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	OverlappingCharacter = nullptr;
+	bConsumed = false;
 }
 
-// Called when the game starts or when spawned
+
 void APickup_Weapon::BeginPlay()
 {
 	Super::BeginPlay();
+	SphereCollision->OnComponentBeginOverlap.AddDynamic(this, &APickup_Weapon::OnBeginOverlap);
+	SphereCollision->OnComponentEndOverlap.AddDynamic(this, &APickup_Weapon::OnEndOverlap);
 	
 }
 
-// Called every frame
-void APickup_Weapon::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
+void APickup_Weapon::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (bConsumed) return;
+	if (AMainCharacter* MainCharacter = Cast<AMainCharacter>(OtherActor))
+	{
+		OverlappingCharacter = MainCharacter;
+	}
+}
+
+void APickup_Weapon::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (AMainCharacter* MainCharacter = Cast<AMainCharacter>(OtherActor))
+	{
+		if (OverlappingCharacter == MainCharacter)
+		{
+			OverlappingCharacter = nullptr;
+		}
+	}
+}
+
+bool APickup_Weapon::TryConsume(AMainCharacter* Interactor)
+{
+	if (bConsumed || !Interactor || !WeaponClass) return false;
+	if (OverlappingCharacter && OverlappingCharacter != Interactor) return false;
+
+	if (UInventoryComponent* Inventory = Interactor->GetInventoryComponent())
+	{
+		const bool bAdded = Inventory->AddOrReplaceWeapon(WeaponClass);
+		if (bAdded)
+		{
+			bConsumed = true;
+			Destroy();
+			return true;
+		}
+	}
+	return false;
 }
 
