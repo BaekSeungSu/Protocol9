4ba5b041493ed105ce6bcee74b7bb4fc52e427ab
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-08-18 12:10:57 +0900

    캐릭터 공격 관련 로직 수정 및 LevelUpAttack 파라미터 비활성화

--- Source/Protocol9/Public/Character/MainCharacter.h
+++ Source/Protocol9/Public/Character/MainCharacter.h
@@ -46,8 +46,8 @@ protected:
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Attack")
 	float BasetAttack;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Attack")
-	float LevelUpAttack;
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Attack")
+	float LevelUpAttack;*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Attack")
 	float Attack;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Attack")
@@ -155,11 +155,12 @@ public:
 	void SetupDeathCamera();
 	void ResetCameraToDefault();
 	
-	int GetAttack() const{return Attack;}
+	float GetAttack() const{return CurrentAttack;}
+	float GetAttackBonus() const { return (CurrentAttack/5.f) * 0.1f;}
 	int GetExp() const {return Exp;}
 	int GetCharacterLevel() const {return CharacterLevel;}
 
-	void SetAttack(int NewAttack);
+	void SetAttack(float NewAttack);
 	void SetExp(int Exp);
 	void SetLevel(int Level);
 	void AddExp(int NewExp);
